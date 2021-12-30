36e7866f0420d8068f38153f65218bbc15b727f3
rovelee <rovelee223@163.com>
2021-12-30 20:14:27 +0800

    update quadratic function

--- step7/quadratic.c
+++ step7/quadratic.c
@@ -12,7 +12,26 @@ void QuadraticEquation(double a, double b, double c,
                        double *pZ1r, double *pZ1i, double *pZ2r, double *pZ2i)
 {
     /* Insert function body here */
-    // TODO
+    
+    double discriminant;
+    // 二次方程判别式 b的平方减4ac
+    discriminant = b*b-4*a*c;
+    /* 
+        二次方程通解：
+            当判别式大于等于0时，虚部为0，实部等于-b加减根号下判别式除以2a
+            当判别式小于0时，分解分式，实部为-b除以2a，虚部为正负根号下判别式除以2a
+    */
+    if(discriminant >= 0){
+        *pZ1i = 0;
+        *pZ2i = 0;
+        *pZ1r = (-b + sqrt(discriminant)) / (2 * a);
+        *pZ2r = (-b + sqrt(discriminant)) / (2 * a);
+    }else{
+        *pZ1i = sqrt(-discriminant) / (2 * a);
+        *pZ2i = -sqrt(-discriminant) / (2 * a);
+        *pZ1r = -b / (2 * a);
+        *pZ2r = *pZ1r;
+    }
 }
 /* 
  * Input a quadratic equation as a, b, and c
@@ -34,7 +53,7 @@ int main()
     double a, b, c;
     double z1r, z1i;    /* First zero */
     double z2r, z2i;    /* Second zero */
-    double discriminant;
+    // double discriminant;
     
     // printf("Input a: ");
     // scanf("%lf", &a);
@@ -51,26 +70,26 @@ int main()
      */
      
     /* Compute the discriminant */
-    discriminant = b * b - 4 * a * c;
-    if(discriminant >= 0)
-    {
-        /* If the discriminant is greater than or
-           equal to zero, the zeros are real */
-        z1r = (-b + sqrt(discriminant)) / (2 * a);
-        z2r = (-b - sqrt(discriminant)) / (2 * a);
-        z1i = 0;
-        z2i = 0;
-    }
-    else
-    {
-        /* If the discriminant is less than zero
-           the zeros are complex  */
-        z1r = -b / (2 * a);
-        z2r = z1r;
-        z1i = sqrt(-discriminant) / (2 * a);
-        z2i = -sqrt(-discriminant) / (2 * a);
-    }
-    
+    // discriminant = b * b - 4 * a * c;
+    // if(discriminant >= 0)
+    // {
+    //     /* If the discriminant is greater than or
+    //        equal to zero, the zeros are real */
+    //     z1r = (-b + sqrt(discriminant)) / (2 * a);
+    //     z2r = (-b - sqrt(discriminant)) / (2 * a);
+    //     z1i = 0;
+    //     z2i = 0;
+    // }
+    // else
+    // {
+    //     /* If the discriminant is less than zero
+    //        the zeros are complex  */
+    //     z1r = -b / (2 * a);
+    //     z2r = z1r;
+    //     z1i = sqrt(-discriminant) / (2 * a);
+    //     z2i = -sqrt(-discriminant) / (2 * a);
+    // }
+    QuadraticEquation(a,b,c,&z1r,&z1i,&z2r,&z2i);   
     /* Display the results */
     printf("Zero 1: %f + %fj\n", z1r, z1i);
     printf("Zero 2: %f + %fj\n", z2r, z2i);
